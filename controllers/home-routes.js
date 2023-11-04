81eefe6cc476b113135528c0599aa2615591d904
kyleespera <kyleespera134@gmail.com>
2023-11-03 19:01:16 -0700

    Replaced then/catch with async/await for handling

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,44 +1,51 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
-  Post.findAll({
-    attributes: [
-      'id',
-      'title',
-      "post_text",
-      'created_at'      
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      // pass a single post object into the homepage template
-      res.render('homepage', { 
-        posts,
-        loggedIn: req.session.loggedIn 
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+// Helper function to serialize data
+const serializeData = (data) => data.get({ plain: true });
+
+// Attributes to be retrieved for posts, users, and comments
+const postAttributes = ['id', 'title', 'post_text', 'created_at'];
+const userAttributes = ['username'];
+const commentAttributes = ['id', 'comment_text', 'post_id', 'user_id', 'created_at'];
+
+// Include options for post queries
+const includeOptions = [
+  {
+    model: Comment,
+    attributes: commentAttributes,
+    include: {
+      model: User,
+      attributes: userAttributes
+    }
+  },
+  {
+    model: User,
+    attributes: userAttributes
+  }
+];
+
+// Error handling function
+const handleErrors = (err, res) => {
+  console.error(err);
+  res.status(500).json(err);
+};
+
+// GET all posts to display on the homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      attributes: postAttributes,
+      include: includeOptions
     });
+    const posts = dbPostData.map(serializeData);
+    res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    handleErrors(err, res);
+  }
 });
 
+// GET the login page, redirect if user is already logged in
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -47,56 +54,25 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'title',
-      'post_text',
-      'created_at'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: [
-          'id',
-          'comment_text',
-          'post_id',
-          'user_id',
-          'created_at'
-        ],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-  .then(dbPostData => {
+// GET a single post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const dbPostData = await Post.findOne({
+      where: { id: req.params.id },
+      attributes: postAttributes,
+      include: includeOptions
+    });
+
     if (!dbPostData) {
       res.status(404).json({ message: 'No Post found with this id' });
       return;
     }
-    //serialize the data
-    const post = dbPostData.get({ plain: true });
 
-    //pass data to the template
-    res.render('single-post', {
-      post, 
-      loggedIn: req.session.loggedIn
-    });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    const post = serializeData(dbPostData);
+    res.render('single-post', { post, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    handleErrors(err, res);
+  }
 });
 
 module.exports = router;
