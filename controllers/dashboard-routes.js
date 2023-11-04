0bcd3883c2a24ae4beb79e174f4658d91f4855af
kyleespera <kyleespera134@gmail.com>
2023-11-03 18:59:39 -0700

    revised the asynchronous calls to use async/await

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,84 +2,86 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get all posts
-router.get('/', withAuth, (req, res) => {
-  Post.findAll({
-    where: {
-      user_id: req.session.user_id
-    },
-    attributes: ['id', 'title', 'post_text', 'created_at'],
-    order: [['created_at', 'DESC']],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      }
-    ]
-  })
-  .then(dbPostData => {
-    //serialize the data before passing to the template
-    const posts = dbPostData.map(post => post.get({ plain: true }));
-    res.render('dashboard', { posts, loggedIn: true });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
+// Helper function to serialize data
+const serializeData = data => data.map(post => post.get({ plain: true }));
 
-//get a single post
-router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: ['id', 'title', 'post_text', 'created_at'],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
+// Handler for errors
+const errorHandler = (err, res) => {
+  console.error(err);
+  res.status(500).json(err);
+};
+
+// Attributes to be retrieved for posts
+const postAttributes = ['id', 'title', 'post_text', 'created_at'];
+
+// Attributes to be retrieved for users
+const userAttributes = ['username'];
+
+// Attributes to be retrieved for comments
+const commentAttributes = ['id', 'comment_text', 'post_id', 'user_id', 'created_at'];
+
+// Include options for post queries
+const includeOptions = [
+  {
+    model: User,
+    attributes: userAttributes
+  },
+  {
+    model: Comment,
+    attributes: commentAttributes,
+    include: {
+      model: User,
+      attributes: userAttributes
+    }
+  }
+];
+
+// GET all posts
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
       },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      //serialize the data
-      const post = dbPostData.get({ plain: true });
-      // pass to the template
-      res.render('edit-post', {
-        post,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+      attributes: postAttributes,
+      order: [['created_at', 'DESC']],
+      include: includeOptions
     });
+
+    const posts = serializeData(dbPostData);
+    res.render('dashboard', { posts, loggedIn: true });
+  } catch (err) {
+    errorHandler(err, res);
+  }
 });
 
-router.get('/new', (req, res) => {
-  res.render('new-post');
+// GET a single post for editing
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: postAttributes,
+      include: includeOptions
+    });
+
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = dbPostData.get({ plain: true });
+    res.render('edit-post', { post, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    errorHandler(err, res);
+  }
 });
 
+// GET the page to create a new post
+router.get('/new', withAuth, (req, res) => {
+  // You might want to add authentication here if creating a post requires a user to be logged in.
+  res.render('new-post', { loggedIn: req.session.loggedIn });
+});
 
 module.exports = router;
