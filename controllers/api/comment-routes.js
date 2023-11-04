be4d48ec2eb63ece3ae333fa460ec97cfec58e43
kyleespera <kyleespera134@gmail.com>
2023-11-03 19:14:57 -0700

    asynchronous ops are handled with async/await.

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,58 +2,53 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// Helper function for handling errors
-const handleErrors = (res, err) => {
-  console.error(err); // Using console.error for logging errors is a better practice
-  res.status(500).json({
-    message: 'An error occurred', // It's good to provide a generic error message rather than exposing the error details
-    error: err.toString(), // Send a string version of the error for client-side handling if needed
-  });
-};
-
-// Route to get all comments
+// Get all comments
 router.get('/', async (req, res) => {
   try {
     const dbCommentData = await Comment.findAll();
     res.json(dbCommentData);
   } catch (err) {
-    handleErrors(res, err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
-// Route to create a new comment with authentication
+// Post a new comment
 router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = {
-      comment_text: req.body.comment_text,
-      post_id: req.body.post_id,
-      // Ensuring that user_id is obtained from the session which is supposed to be set after authentication
-      user_id: req.session.user_id,
-    };
-
-    const dbCommentData = await Comment.create(newComment);
-    res.status(201).json(dbCommentData); // Respond with 201 status code on successful creation
-  } catch (err) {
-    handleErrors(res, err);
+  // Check for a session
+  if (req.session) {
+    try {
+      const dbCommentData = await Comment.create({
+        comment_text: req.body.comment_text,      
+        post_id: req.body.post_id,
+        user_id: req.session.user_id
+      });
+      res.json(dbCommentData);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
   }
 });
 
-// Route to delete a comment by id
-router.delete('/:id', withAuth, async (req, res) => { // Assuming deletion should also be protected by auth
+// Delete a comment
+router.delete('/:id', withAuth, async (req, res) => {
   try {
     const dbCommentData = await Comment.destroy({
       where: {
-        id: req.params.id,
-      },
+        id: req.params.id
+      }
     });
 
     if (!dbCommentData) {
-      return res.status(404).json({ message: 'No comment found with this id' });
+      res.status(404).json({ message: 'No comment found with this id' });
+      return;
     }
-
-    res.status(200).json({ message: 'Comment deleted successfully' }); // Send a confirmation message on successful deletion
+    
+    res.json({ message: 'Comment deleted' });
   } catch (err) {
-    handleErrors(res, err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
