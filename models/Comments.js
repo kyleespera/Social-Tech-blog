f1d38704bf434eff60eb9eab0a75a3bb5cf5cd56
kyleespera <kyleespera134@gmail.com>
2023-11-03 23:53:41 -0700

    freezeTableName set true

--- models/Comments.js
+++ models/Comments.js
@@ -1,44 +1,48 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+// Use a class declaration for the model which extends Sequelize's Model class
 class Comment extends Model {}
 
+// Initialize the model's schema and configuration
 Comment.init(
   {
+    // Define attributes
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     comment_text: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1]
-      }
+        len: [1], // Ensures the comment is not empty
+      },
     },
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'user',
-        key: 'id'
-      }
+        model: 'user', // This should match the table name for the user model
+        key: 'id',
+      },
     },
     post_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'post',
-        key: 'id'
-      }
-    }
+        model: 'post', // This should match the table name for the post model
+        key: 'id',
+      },
+    },
   },
   {
+    // Sequelize model options
     sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment'
+    freezeTableName: true, // Prevents Sequelize from renaming the table
+    underscored: true, // Ensures that all attributes are named with underscores instead of camelCase
+    modelName: 'comment', // This is the name to use when associating with other models
   }
 );
 
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
@@ -7,36 +9,42 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
-require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//setup session
+// Set up session with environment variable for secret
 const sess = {
-  secret: 'supersecretsessionsecrettext',
-  cookie: { maxAge: 180000},
+  secret: process.env.SESSION_SECRET, // Make sure to set SESSION_SECRET in your .env file
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
-//handlebars initialization
+app.use(session(sess));
+
+// Handlebars setup with custom helpers
 const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session(sess));
-
-//use routes
+// Use routes
 app.use(routes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server after database synchronization
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
 });
